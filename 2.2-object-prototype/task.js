6706e670ddbe0249b367e1a1e1947759c7fc3dfb
Kononova Daria <kononova.yu.daria@mail.ru>
2021-04-19 18:28:05 +0300

    Решение №2.2

--- 2.2-object-prototype/task.js
+++ 2.2-object-prototype/task.js
@@ -1,11 +1,54 @@
-//String.prototype.isPalindrome - для задачи №1
+//Задача №1
 
-function getAverageMark(marks) {
-    // код для задачи №2 писать здесь
-    // return averageMark
+String.prototype.isPalindrome = function() {
+	let phrase = this.toUpperCase().split("");
+	let phraseWithoutSpaces = [];
+	let phraseReverse = [];
+
+	for (let i = 0; i < phrase.length; i++) {
+		if (phrase[i] !== " ") {
+			phraseWithoutSpaces.push(phrase[i]);
+			phraseReverse.unshift(phrase[i]);
+		}
+	}
+
+	let phraseReverseStr = phraseReverse.join("");
+	let phraseWithoutSpacesStr = phraseWithoutSpaces.join("");
+
+	if (phraseReverseStr === phraseWithoutSpacesStr) {
+		return true;
+	} else {
+		return false;
+	}
 }
 
+// Задача №2
+
+function getAverageMark(marks) {
+    let sum = 0;
+
+    for (let i = 0; i < marks.length; i++) {
+    	sum = sum + marks[i];
+    }
+
+    if (sum === 0) {
+    	return 0;
+    } else {
+    	let averageMark = sum / marks.length;
+    	return Math.round(averageMark);
+    }
+}
+
+//Задача №3
+
 function checkBirthday(birthday) {
-    // код для задачи №3 писать здесь
-    // return verdict
+    let now = Date.now();
+    birthday = +new Date(birthday);
+    let diff = now - birthday;
+    let age = diff / (86400000 * 365.25);
+    if (age >= 18) {
+    	return true;
+    } else {
+    	return false;
+    }
 }
